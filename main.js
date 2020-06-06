492e84e9053493cfe9afcb105a660a09ac93e0fd
Ishan Sharma <ishandeveloper@outlook.com>
2020-06-07 00:25:48 +0530

    Final

--- main.js
+++ main.js
@@ -1,50 +1,39 @@
-// Refer to the form element
 const form = document.querySelector(".search-form");
-
-// Add event handler to form submission
 form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-  // prevent page from reloading when form is submitted
-  event.preventDefault();
-  // get the value of the input field
-  const input = document.querySelector(".search-input").value;
-  // remove whitespace from the input
-  const searchQuery = input.trim();
-  // print `searchQuery` to the console
-  fetchResults(searchQuery);
+function handleSubmit(e) {
+  e.preventDefault();
+  let query = document.querySelector(".search-input").value;
+  query = query.trim();
+  getResults(query);
 }
 
-function fetchResults(searchQuery) {
-  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=25&srsearch=${searchQuery}`;
+function getResults(query) {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&origin=*&srlimit=25&utf8=&format=json&srsearch=${query}`;
 
-  fetch(endpoint)
-    .then((response) => response.json())
+  fetch(url)
+    .then((res) => res.json())
     .then((data) => {
-      const results = data.query.search;
-      displayResults(results);
+      putResults(data.query.search);
     })
-    .catch(() => console.log("An error occurred"));
+    .catch((e) => console.log(`ERROR : ${e}`));
 }
 
-function displayResults(results) {
-  // Store a reference to `.searchResults`
+function putResults(sResults) {
   const searchResults = document.querySelector(".results");
-  // Remove all child elements
   searchResults.innerHTML = "";
-  // Loop over results array
-  results.forEach((result) => {
+  sResults.forEach((result) => {
     const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
 
     searchResults.insertAdjacentHTML(
       "beforeend",
       `<div class="result">
-        <h3 class="result-title">
-          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
-        </h3>
-        <span class="result-snippet">${result.snippet}</span><br>
-        <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
-      </div>`
+      <h3 class="result-title">
+        <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+      </h3>
+      <span class="result-snippet">${result.snippet}</span><br>
+      <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+    </div>`
     );
   });
 }
